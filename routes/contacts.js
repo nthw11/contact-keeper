d145b9feef485c7696388df33399bae851a32b91
Nathaniel Wallace <nathaniel.wallace@gmail.com>
2021-01-12 10:59:47 -0500

    started project
    routes, server up and running

---
+++ routes/contacts.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/contacts
+// @desc    Get all users contacts
+// @access  Private
+router.get('/', (req, res) => {
+  res.send('get all contacts');
+});
+
+// @route   POST api/contacts
+// @desc    Get all users contacts
+// @access  Private
+router.post('/', (req, res) => {
+  res.send('add new contact');
+});
+
+// @route   PUT api/contacts/:id
+// @desc    Update contact
+// @access  Private
+router.put('/:id', (req, res) => {
+  res.send('update contact');
+});
+
+// @route   DELETE api/contacts/:id
+// @desc    Delete contact
+// @access  Private
+router.delete('/:id', (req, res) => {
+  res.send('delete contact');
+});
+
+module.exports = router;
